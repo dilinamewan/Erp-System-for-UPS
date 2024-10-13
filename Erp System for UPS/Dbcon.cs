249068db43261c113dec4a7a876c9841289a1b86
dilinamewan <dilinamewan07@gmail.com>
2024-10-13 19:14:19 +0530

    Revert "Merge branch 'Package-Tracking-+-Fleet-Management' of https://github.com/dilinamewan/Erp-System-for-UPS into Package-Tracking-+-Fleet-Management"
    This reverts commit 6888a023c6937aa730a63167dfb1f118562ab169, reversing
    changes made to 8bb86a17af98a35b2795fc1ca3689bc199298619.

--- Erp System for UPS/Dbcon.cs
+++ Erp System for UPS/Dbcon.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
-using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 public class Dbcon
@@ -110,4 +109,3 @@ public class Dbcon
         return await ExecuteQuery(query);
     }
 }
-
