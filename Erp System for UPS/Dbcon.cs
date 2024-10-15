20bdad4dea95f569ccdfe467d9e36d4258b2df24
tdulshan3 <135942637+tdulshan3@users.noreply.github.com>
2024-10-15 23:31:53 +0530

    update dbcon

--- Erp System for UPS/Dbcon.cs
+++ Erp System for UPS/Dbcon.cs
@@ -15,26 +15,26 @@ public class Dbcon
         conn = null;
     }
 
-    // Connect to the database
+
     public async Task Connect()
     {
-        string connectionString = "Server=localhost;Port=3306;Database=ups;Uid=root;Pwd=;";
+        string connectionString = "Server=localhost;Port=3306;Database=ups;Uid=root;Pwd=root;";
         conn = new MySqlConnection(connectionString);
         await conn.OpenAsync();
     }
 
-    // Disconnect the database
+
     public void Disconnect()
     {
         conn?.Close();
     }
 
-    // Execute a SELECT query and return a data reader
+  
     public async Task<MySqlDataReader> ExecuteQuery(string query, Dictionary<string, object> parameters = null)
     {
         MySqlCommand cmd = new MySqlCommand(query, conn);
 
-        // If parameters are provided, add them to the command
+       
         if (parameters != null)
         {
             foreach (var param in parameters)
@@ -47,12 +47,12 @@ public class Dbcon
         return (MySqlDataReader)reader;
     }
 
-    // Execute a non-query (INSERT, UPDATE, DELETE) and return the number of affected rows
+    
     public async Task<int> ExecuteNonQuery(string query, Dictionary<string, object> parameters = null)
     {
         using (var command = new MySqlCommand(query, conn))
         {
-            // If parameters are provided, add them to the command
+       
             if (parameters != null)
             {
                 foreach (var param in parameters)
@@ -65,12 +65,12 @@ public class Dbcon
         }
     }
 
-    // Execute a scalar query and return a single value (useful for retrieving auto-generated IDs)
+   
     public async Task<object> ExecuteScalar(string query, Dictionary<string, object> parameters = null)
     {
         using (var command = new MySqlCommand(query, conn))
         {
-            // If parameters are provided, add them to the command
+           
             if (parameters != null)
             {
                 foreach (var param in parameters)
