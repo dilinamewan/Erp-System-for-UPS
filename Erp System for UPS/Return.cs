50f2666a0b517475394527b1476f5221d9901ca4
SandunSandeepa <sandunsandeepa567@gmail.com>
2024-10-15 16:47:31 +0530

    return form

--- Erp System for UPS/Return.cs
+++ Erp System for UPS/Return.cs
@@ -17,34 +17,5 @@ namespace Erp_System_for_UPS
             InitializeComponent();
         }
 
-        private void Return_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label4_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox4_TextChanged(object sender, EventArgs e)
-        {
-
-        }
     }
 }
